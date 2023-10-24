2647bdd0c8908dbbd2849bfdfc94f9b5d2a1f525
Sergey <smirsand@mail.ru>
2023-10-24 20:15:02 +0300

    add urls.py config

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('api/', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
+
 ]
