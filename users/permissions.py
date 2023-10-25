4233ca0ea87cb1f9473280800f101f77a0f6b705
Sergey <smirsand@mail.ru>
2023-10-25 21:06:55 +0300

    add code permissions.py users

--- users/permissions.py
+++ users/permissions.py
@@ -1,21 +1,31 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsModeratorBan(BasePermission):
+class IsModeratorBanLesson(BasePermission):
     """
-    Запрет модераторам.
+    Запрет модераторам для создания/удаления уроков.
     """
     def has_permission(self, request, view):
         if request.user.groups.filter(name='Модераторы').exists():
-            return False
+            return request.method in ['GET', 'HEAD', 'PUT']
         return True
 
 
-class IsModeratorOrReadUpdateOnly(BasePermission):
+class IsOwnerOfStaff(BasePermission):
     """
-    Запрет модераторам на создание/удаление курса и урока.
+    Проверяет, является ли текущий пользователь владельцем объекта или персоналом.
+    """
+    def has_permission(self, request, view):
+        user = request.user
+
+        return view.queryset.filter(owner=user).exists() or user.is_staff
+
+
+class IsModeratorReadOnlyUpdate(BasePermission):
+    """
+    Запрет модераторам на создание/удаление курса.
     """
     def has_permission(self, request, view):
         if request.user.groups.filter(name='Модераторы').exists():
-            return request.method in ['GET', 'HEAD', 'PUT', 'PATCH']
-        return False
+            return request.method in ['GET', 'HEAD', 'PUT']
+        return True
