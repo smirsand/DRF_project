c43330c4c6e44e0b04fa6dda9fbfb4ff79626cce
Sergey <smirsand@mail.ru>
2023-11-01 11:57:17 +0300

    add code settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
