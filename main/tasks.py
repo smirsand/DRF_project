c3c0f6a4b22015eb5b0645b24a6756e9b073edb5
Sergey <smirsand@mail.ru>
2023-11-06 21:25:24 +0300

    add code tasks.py main

--- main/tasks.py
+++ main/tasks.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.mail import send_mail
 
-from users.models import User
 from .models import Course
 
 
@@ -19,3 +21,18 @@ def send_course_update_emails(course_id, *args, **kwargs):
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[user.email for user in User.objects.all()],  # получатели писем,
     )
+
+
+def user_blocking_task():
+    """
+    Блокирует пользователя если он не был активен 30 дней.
+    """
+
+    users_list = User.objects.all()
+    today = datetime.utcnow()
+    threshold = today - timedelta(days=30)
+
+    for user in users_list:
+        if user.is_active and user.last_login <= threshold:
+            user.is_active = False
+            user.save()
